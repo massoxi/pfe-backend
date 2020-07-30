288750f3bd4c6b142afeaab83061a75d56dd8788
massoxi <mlau09@outlook.fr>
2020-07-30 10:54:21 +0200

    .

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -8,7 +8,6 @@ class Query(user_query, graphene.ObjectType):
 
 
 class Mutation(user_mutation, graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
